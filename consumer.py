b2a0b2980f059f824fd847c74f39e237743004f4
Samridh Raj <samcodes.148@gmail.com>
2026-09-21 01:16:22 +0530

    Implement Kafka consumer for server metrics

---
+++ consumer.py
@@ -0,0 +1,29 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    "server_metrics",
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    group_id="aiops-monitor",
+    value_deserializer=lambda value: json.loads(value.decode("utf-8"))
+)
+
+print("Waiting for messages...")
+
+for message in consumer:
+
+    data = message.value
+
+    server = data["server_id"]
+    cpu = data["cpu_usage"]
+    memory = data["memory_usage"]
+
+    print("\nReceived:")
+    print("Server:", server)
+    print("CPU:", cpu, "%")
+    print("Memory:", memory, "%")
+
+    if cpu > 80:
+        print("ALERT: High CPU detected on", server)
